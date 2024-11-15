0c2606171e2a3cd9b23b143cfda7fb54723642c4
Zanexywex <thanarat.wex@gmail.com>
2024-11-15 19:34:06 +0700

    Update Skynet-Loader.lua

--- Skynet-Loader.lua
+++ Skynet-Loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 16732694052 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/Fisch.php"))()
+elseif game.PlaceId == 142823291 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/mm2.html"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/GPO.js"))()
 end
