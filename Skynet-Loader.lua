21ec01fae74bc94b4efc3703ed9bd5f12677d99b
Zanexywex <thanarat.wex@gmail.com>
2025-03-04 12:16:21 +0700

    Update Skynet-Loader.lua

--- Skynet-Loader.lua
+++ Skynet-Loader.lua
@@ -6,6 +6,8 @@ elseif game.PlaceId == 10450270085 or game.PlaceId == 16379688837 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/JUJUTSU.roblox"))()
 elseif game.PlaceId == 99995671928896 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/Rune%20Slayer"))()
+elseif game.GameId == 4658598196 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/AOTR.script.wex.skynet.lol.com.x"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Zanexywex/Skynet-x-WexTeam/refs/heads/main/GPO.js"))()
 end
